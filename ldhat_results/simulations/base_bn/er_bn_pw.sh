166e91aa71e397556f4a786eaa3751f563f5ebdd
sjsabin <susanna.sabin@gmail.com>
2022-02-14 14:51:51 -0500

    Add er_bn_pw.sh script

---
+++ ldhat_results/simulations/base_bn/er_bn_pw.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Execution script for running LDhat pairwise on numerous simulation models.
+# Usage:
+#	./er_bn_pw.sh [0-99]
+
+# Load stashcache
+#module load stashcache
+
+
+# Extract and build LDHat software locally
+tar xf LDhat-master.tar.gz
+cd LDhat-master
+make
+cd ../
+# Copy archive of fastas from the public directory to the host directory
+#stashcp /osgconnect/public/sjsabin/er_bn_convert_out.tar.gz ./
+# Extract the fasta files
+tar xf er_bn_convert_out.tar.gz
+
+REP=er_bn.$1
+
+echo "0.002264 100 201 0 0 1 1 1" | LDhat-master/pairwise -seq er_bn_convert_out/${REP}.sites.txt -loc er_bn_convert_out/${REP}.locs.txt -prefix ${REP}. >> ${REP}.pw.log
+
+# Remove the large input file from the host directory
+rm -r er_bn_convert_out/
+rm -r LDhat-master/
