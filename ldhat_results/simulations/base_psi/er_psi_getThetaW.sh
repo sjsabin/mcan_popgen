e22967bb88b75eb2b7565b530db6703446a48f93
sjsabin <susanna.sabin@gmail.com>
2022-02-14 14:56:30 -0500

    Remove getThetaW script

--- ldhat_results/simulations/base_psi/er_psi_getThetaW.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-BASES=413587
-THETA=$(grep "Watterson" er_psi.${1}.cons.convert.log | awk '{split($0,a," ") ; print a[4]}')
-THETA_pb=$(awk -v NUM=${THETA} -v BASES=${BASES} 'BEGIN { print (NUM/BASES)}')
-echo -e $1"\t"$THETA_pb >> er_psi.consensus_pairwise_args.txt
